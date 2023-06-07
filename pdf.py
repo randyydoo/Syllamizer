5b516283d11645f6079de2f073e787c807901f02
Randy Do <randyydoo@csu.fullerton.edu>
2023-06-06 19:22:07 -0700

    changed to pdf2docx

--- pdf.py
+++ pdf.py
@@ -1,16 +1,6 @@
-import pdfplumber
+from pdf2docx import Converter
 
-pdf_name = 'insert/pdf/name/here'
-
-def get_table():
-    table = {}
-    pdf = pdfplumber.open(f'{pdf_name}.pdf')
-    first = pdf.pages[0]
-    lists = first.extract_table()
-    print(lists)
-    for i in range(len(lists) - 1):
-        for j in range(len(lists[i])):
-            title = lists[i][j]
-            text = lists[i + 1][j]
-            table[title] = text
-    return table
+def convert(pdf_name, docx_name):
+    cv = Converter(pdf_name)
+    new = cv.convert(docx_name)      # all pages by default
+    cv.close()
