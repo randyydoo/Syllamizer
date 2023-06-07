f2215796f2b163450c1cae3104ce4016f6ba7d80
Randy Do <randyydoo@csu.fullerton.edu>
2023-06-06 19:21:45 -0700

    created user input

---
+++ main.py
@@ -0,0 +1,15 @@
+import pdf
+
+
+user_file_type = input("Is you syllabus a docx file? (y/n): ")
+
+if user_file_type == 'y' or user_file_type == 'Y':
+    docx_name = input('Please input the docx name and make sure it is in the same directory: ')
+    docx = f'{docx_name}.docx'
+else:
+    pdf_name = input('Please input the pdf file name and make sure it is in the same directory: ')
+    convert_file = f'{pdf_name.pdf}'
+    docx = f'{pdf_name}.docx' 
+    pdf.convert(f'{pdf_name}.pdf', docx)
+
+
