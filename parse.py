d5417513469ddde8fe2fb973a5b36ee1ff98c79f
Randy Do <randyydoo@csu.fullerton.edu>
2023-06-20 18:20:18 -0700

    algo for rows done

--- parse.py
+++ parse.py
@@ -7,23 +7,39 @@ def is_bold(cell: 'class') -> bool:
             if not run.bold:
                 return False
     return True
-def get_tables(docx: str) -> None:
-    keys = []  #list of lists list of tables and contents
-    contents = []
+def get_keys_tables(docx: str) -> list[list[str]]:
+    redact = ['Grade', 'Percentage']
+    keys = []  
+    contents = [] 
     doc = Document(docx)
 
     for table in doc.tables:
-        firstRow = table.rows[0] 
-        temp = []
-        bold = True
-        for cell in firstRow.cells:
-            if is_bold(cell):
-                temp.append(cell.text)
-        if len(temp) != 0:
-            keys.append(temp)
+        contentTemp, keyTemp = [], []
+        bold, skip= True, False
+        for i, row in enumerate(table.rows):
+            temp = []
+            for cell in row.cells:
+                if cell.text in redact:
+                    skip = True
+                if is_bold(cell) and i == 0:
+                    keyTemp.append(cell.text)
+                elif len(keyTemp) != 0:
+                    if cell.text == '':
+                        temp.append('None')
+                    else:
+                        temp.append(cell.text)
+            if len(temp) != 0:
+                contentTemp.append(temp)
+        if len(contentTemp) != 0 and not skip:
+            contents.append(contentTemp)
+        if len(keyTemp) != 0 and not skip:
+            keys.append(keyTemp)
     print(keys)
+    print(len(contents))
+    print(contents)
 
-def get_title(docx: str) -> str:
+# def get_content_tables(docx: str) -> list[list[str]]:
+
 
 def get_text(docx: str) -> None:
     # read text starting from bold and end when new bold word
@@ -34,4 +50,4 @@ def get_text(docx: str) -> None:
         count += 1
         if count == 3:
             return
-get_tables('240.docx')
+get_keys_tables('362.docx')
