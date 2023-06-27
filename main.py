b381d45d53336274cf2d39b5cdbd226f730a123a
Randy Do <randyydoo@csu.fullerton.edu>
2023-06-27 16:28:45 -0700

    cleaned up code

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import pdf
 
+def get_file_name():
+    user_file_type = input("Is you syllabus a docx file? (y/n): ")
 
-user_file_type = input("Is you syllabus a docx file? (y/n): ")
-
-if user_file_type == 'y' or user_file_type == 'Y':
-    docx_name = input('Please input the docx name and make sure it is in the same directory: ')
-    docx = f'{docx_name}.docx'
-else:
-    pdf_name = input('Please input the pdf file name and make sure it is in the same directory: ')
-    convert_file = f'{pdf_name}.pdf'
-    docx = f'{pdf_name}.docx' 
-    pdf.convert(f'{pdf_name}.pdf', docx)
+    if user_file_type == 'y' or user_file_type == 'Y':
+        docx_name = input('Please input the docx name and make sure it is in the same directory: ')
+        docx = f'{docx_name}.docx'
+    else:
+        pdf_name = input('Please input the pdf file name and make sure it is in the same directory: ')
+        convert_file = f'{pdf_name}.pdf'
+        docx = f'{pdf_name}.docx' 
+        pdf.convert(f'{pdf_name}.pdf', docx)
--- parse.py
+++ parse.py
@@ -1,7 +1,7 @@
 from docx import Document
 import PyPDF2
 
-redact = ['Grade', 'Percentage' 'A:', 'A-']
+redact = ['Grade', 'Percentage', 'A:', 'A-']
 def is_bold(cell: 'class') -> bool:
     for paragraph in cell.paragraphs:
         for run in paragraph.runs:
@@ -12,70 +12,49 @@ def is_bold(cell: 'class') -> bool:
 def get_keys_tables(docx: str) -> list[list[str]]:
     keys = []  
     contents = [] 
-    doc = Document(docx)    
+    doc = Document(docx)
+
     for table in doc.tables:
         contentTemp, keyTemp = [], []
-        new, extend = False, False
+        newTable, extendTable = False, False
         bold, skip= True, False
+
         for i, row in enumerate(table.rows):
-            temp = []
+            tempRow = []
+
             for j, cell in enumerate(row.cells):
                 if j + i == 0:
                     try:
                         prev = int(contents[-1][-1][0])
                         curr = int(cell.text)
                         if curr - prev == 1:
-                            extend = True
+                            extendTable = True
                     except:
+
                         try:
                             curr = int(cell.text)
                             if curr <= 20:
-                                new = True
+                                newTaable = True
                         except:
                             continue
+
                 if cell.text in redact:
                     skip = True
                 if is_bold(cell) and i == 0:
                     keyTemp.append(cell.text)
-                elif len(keyTemp) != 0 or new is True or extend is True:
+                elif len(keyTemp) != 0 or newTable is True or extendTable is True:
                     if cell.text == '':
-                        temp.append('None')
+                        tempRow.append('None')
                     else:
-                        temp.append(cell.text)
-            if extend is True and len(temp) > 0:
-                contents[-1].append(temp)
-            elif len(temp) != 0:
-                contentTemp.append(temp)
+                        tempRow.append(cell.text)
+            if extendTable is True and len(tempRow) > 0:
+                contents[-1].append(tempRow)
+            elif len(tempRow) != 0:
+                contentTemp.append(tempRow)
         if len(contentTemp) != 0 and not skip:
             contents.append(contentTemp)
         if len(keyTemp) != 0 and not skip:
             keys.append(keyTemp)
-    print(len(keys))
-    print(len(contents))
-
-
-
-def get_content_tables(docx: str) -> list[list[str]]:
-    contents = []
-    doc = Document(docx)
-    for table in doc.tables:
-        contentTemp = []
-        skip = False
-        for i, row in enumerate(table.rows):
-            temp = []
-            for cell in row.cells:
-                if i == 0 and cell.text in redact:
-                    skip = True
-                elif cell.text == '' or len(cell.text) == 0:
-                    temp.append('None')
-                else:
-                    temp.append(cell.text)
-            if len(temp) > 0 and skip is False:
-                contentTemp.append(temp)
-        if len(contentTemp) > 0 and skip is False:
-            contents.append(contentTemp)
-    # print(contents)
-    # print(len(contents))
 
 
 def get_text(docx: str) -> None:
