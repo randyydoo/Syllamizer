185410bd5f9c605b0d0a9d76d780cad1ae9144dc
Randy Do <randyydoo@csu.fullerton.edu>
2023-07-06 16:50:37 -0700

    tables finished

--- tables.py
+++ tables.py
@@ -1,21 +1,29 @@
 import pandas as pd
 import parseTables
 
+def get_xlsx(fileName):
+    dFrames = []
+    keys = parseTables.get_keys(fileName)
+    contents = parseTables.get_contents(fileName)
+    for i, table in enumerate(keys):
+        dict = {}
+        for key in table:
+            dict[key] = []
 
-dFrames = []
-
-
-keys = parseTables.get_keys('335.docx')
-contents = parseTables.get_contents('335.docx')
-
-for i, table in enumerate(keys):
-    dict = {}
-    for key in table:
-        dict[key] = []
-    for row in contents[i]:
-        for j, content in enumerate(row):
-            key = keys[i][j]
-            dict[key].append(content)
-    dFrames.append(dict)
+        for row in contents[i]:
+            for j, content in enumerate(row):
+                key = keys[i][j]
+                dict[key].append(content)
+        dFrames.append(dict)
 
 
+
+    writer = pd.ExcelWriter('data.xlsx', engine='xlsxwriter')
+
+    for i, frame in enumerate(dFrames):
+        df = pd.DataFrame(frame)
+        df.to_excel(writer, sheet_name = f'Sheet {i + 1}', index = False)
+
+
+
+get_xlsx('335.docx')
