31b884c581fdcf4303723ef53f415745ece1d97f
Randy Do <randyydoo@csu.fullerton.edu>
2023-07-27 22:45:04 -0700

    model works

--- transformer.py
+++ transformer.py
@@ -1,78 +1,78 @@
-import parseDoc
-import nltk
-import evaluate
-import numpy as np
 from datasets import load_dataset
+from evaluate import load
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, DataCollatorForSeq2Seq, Seq2SeqTrainingArguments, Seq2SeqTrainer
+import nltk
+import numpy as np
 import torch
-from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, Seq2SeqTrainingArguments, Seq2SeqTrainer, DataCollatorForSeq2Seq
 
+dataset = load_dataset("xsum")
+metric = load("rouge")
 
 tokenizer = AutoTokenizer.from_pretrained('t5-small')
-model = AutoModelForSeq2SeqLM.from_pretrained("t5-small")
-data = load_dataset('xsum')   # {splits = 'train', 'validation', 'test'}
-metric = evaluate.load('rouge')
+device = 'mps' if torch.backends.mps.is_available() else "cpu"
+model = AutoModelForSeq2SeqLM.from_pretrained('t5-small')
+model = model.to(device)
 data_collator = DataCollatorForSeq2Seq(tokenizer, model=model)
 
-def get_accuracy(prediction: tuple) -> float:
-    # prediction = (model output, label)
-    outputs, labels = prediction
-    decoded_outs = tokenizer.batch_decode(output, skip_special_tokens=True) 
+def preprocess(examples):
+    inputs = []
+    for doc in examples['document']:
+        text = 'Summarize: ' + doc
+        inputs.append(text)
 
+    model_inputs = tokenizer(inputs, max_length=1024, truncation=True)
+    labels = tokenizer(text_target=examples["summary"], max_length=256, truncation=True)
+
+    model_inputs["labels"] = labels["input_ids"]
+
+    return model_inputs
+
+def compute_metrics(prediction: tuple):
+    #prediction = (model pred, label(target))
+    predictions, labels = prediction
+    decoded_preds = tokenizer.batch_decode(predictions, skip_special_tokens=True)
+    # Replace -100 in the labels as we can't decode them.
     labels = np.where(labels != -100, labels, tokenizer.pad_token_id)
     decoded_labels = tokenizer.batch_decode(labels, skip_special_tokens=True)
-
-    decoded_outs = ["\n".join(nltk.sent_tokenize(pred.strip())) for out in decoded_outs]
-    decoded_labels = ["\n".join(nltk.sent_tokenize(label.strip())) for label in decoded_label]
     
-    result = metric.compute(predictions=decoded_outs, references=decoded_labels, use_stemmer=True, use_aggregator=True)
+    # join scentences
+    decoded_preds = ["\n".join(nltk.sent_tokenize(pred.strip())) for pred in decoded_preds]
+    decoded_labels = ["\n".join(nltk.sent_tokenize(label.strip())) for label in decoded_labels]
+    
+    result = metric.compute(predictions=decoded_preds, references=decoded_labels, use_stemmer=True, use_aggregator=True)
+
     result = {key: value * 100 for key, value in result.items()}
     
-    # Add mean generated length
-    prediction_lens = [np.count_nonzero(pred != tokenizer.pad_token_id) for out in outputs]
+    prediction_lens = [np.count_nonzero(pred != tokenizer.pad_token_id) for pred in predictions]
     result["gen_len"] = np.mean(prediction_lens)
     
     return {k: round(v, 4) for k, v in result.items()}
 
+def train() -> None:
+    tokenized_datasets = dataset.map(preprocess, batched=True)
+    small_train_dataset = tokenized_datasets["train"].shuffle(seed=42).select(range(560))
+    small_eval_dataset = tokenized_datasets["validation"].shuffle(seed=42).select(range(560))
 
-def preprocess(data_set: dict) -> dict: #{input_ids: target_ids}
-    # {'document':  , 'summary':  , 'id': }
-    # inputs = []
-    prefix = 'Summarize: '
-    # for doc in data_set['document']:
-    inputs = [prefix + doc for doc in data_set['document']]
-    #     inputs.append(f'Summarize: {doc}')
+    args = Seq2SeqTrainingArguments(
+        output_dir = 'model-tests',
+        evaluation_strategy = "epoch",
+        learning_rate=2e-5,
+        per_device_train_batch_size=8,
+        per_device_eval_batch_size=8,
+        weight_decay=0.01,
+        save_total_limit=3,
+        num_train_epochs=1,
+    )
 
-    model_inputs = tokenizer(inputs, max_length=1024, truncation=True)
-    
-    with tokenizer.as_target_tokenizer():
-        labels = tokenizer(data_set['summary'], max_length=128, truncation=True)
-
-    model_inputs['decode_input_ids'] = labels['input_ids']
-
-    return model_inputs
-
-tokenized_dataset = data.map(preprocess, batched=True)
-print(tokenized_dataset.keys())
-
-
-training_args = Seq2SeqTrainingArguments(
-    output_dir='model_tests',
-    num_train_epochs=3,     
-    per_device_train_batch_size=8,
-    per_device_eval_batch_size=8,
-    save_steps=1000,            
-    save_total_limit=2,        
-    logging_steps=100,        
-    evaluation_strategy="epoch",
-    eval_steps=1000,        
-)
-trainer = Seq2SeqTrainer(
-            model=model,
-            args=training_args,
-            train_dataset=tokenized_dataset['train'],
-            eval_dataset=tokenized_dataset['validation'],
-            compute_metrics=get_accuracy,
-            data_collator=data_collator,
-            tokenizer=tokenizer
-            )
-trainer.train()
+    trainer = Seq2SeqTrainer(
+        model,
+        args,
+        train_dataset=small_train_dataset,
+        eval_dataset=small_eval_dataset,
+        data_collator=data_collator,
+        tokenizer=tokenizer,
+        compute_metrics=compute_metrics
+    )
+    trainer.train()
+    trainer.save_model('T5-Summarizer-Model')
+train()
