ea6db77f10e4af554f62213b1c6c1fdfc087f0b3
Randy Do <randyydoo@csu.fullerton.edu>
2023-08-15 17:15:20 -0700

    fixed typo for pipeline call

--- torch/generate.py
+++ torch/generate.py
@@ -4,12 +4,11 @@ from transformers import T5Tokenizer, TFT5ForConditionalGeneration, pipeline
 
 def generate_summary(text: str) -> str:
     model_name = './SumModel'
-
-    pipe = pipeline('summarization', model=model_name, max_length=50)
-    summ = pipline(text) 
+    
+    pipe = pipeline('summarization', model=model_name, min_length=100)
+    summ = pipe(text) 
 
     text = summ[0]['summary_text'] + '.'
     text = text.strip()
 
-    return text
-
+    return summ
